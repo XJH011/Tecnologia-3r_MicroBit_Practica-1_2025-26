a48e2776b8451a6dfeeb5e106929883986f4dc3f
夜影、NightShadow <169712528+1224HuangJin@users.noreply.github.com>
2026-03-09 12:24:41 +0100

    ww

--- main.ts
+++ main.ts
@@ -10,4 +10,19 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.pause(100)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("HOLA")
+})
+input.onButtonPressed(Button.B, function () {
+    if (input.temperature() < 20) {
+        basic.showNumber(input.temperature())
+        basic.showIcon(IconNames.Sad)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
+    } else if (input.temperature() < 24) {
+        basic.showNumber(input.temperature())
+        basic.showIcon(IconNames.Happy)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+    }
+    basic.pause(100)
+})
 basic.showString("HOLA")
